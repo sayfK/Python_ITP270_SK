7c1ea539e4cb42c61cc4072c8758307e440a7805
Cyber Range Student <student@ip-10-1-135-31.ec2.internal>
2023-03-22 01:02:11 +0000

    python basics assignment

---
+++ python_basics.sh
@@ -0,0 +1,12 @@
+#!/bin/python3
+
+# Variables
+name = input("What is your name?")
+age = int(input("What is your age?"))
+degree = input("What is your degree program?")
+
+# Functions
+remainingAge = (age*3)/2
+
+# Main Body
+print(name + ", " + str(remainingAge) + ", " + degree)
